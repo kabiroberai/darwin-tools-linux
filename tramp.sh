112288966325434ee6533876316377a06d29d438
Kabir Oberai <oberai.kabir@gmail.com>
2018-09-05 02:21:03 +0530

    Accept clang-7 and clang-6.0 as well

--- tramp.sh
+++ tramp.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 base="$(basename $0)"
-"${0##*-}-8" -target "$(readlink -f "$(dirname "$0")")/${base%-*}" "$@"
+tool="${base##*-}"
+
+if type "${tool}" >/dev/null 2>&1; then
+	:
+elif type "${tool}-8" >/dev/null 2>&1; then
+	tool="${tool}-8"
+elif type "${tool}-7" >/dev/null 2>&1; then
+	tool="${tool}-7"
+elif type "${tool}-6.0" >/dev/null 2>&1; then
+	tool="${tool}-6.0"
+else
+	echo "Could not find clang 6.0 or later in PATH."
+	exit 1
+fi
+
+"${tool}" -target "$(readlink -f "$(dirname "$0")")/${base%-*}" "$@"
